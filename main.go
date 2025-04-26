c175b289777a8b38d0864fe747a1ac713810ec0f
Manuel Braunwald <manuelbrusche@gmail.com>
2025-04-26 19:07:06 +0200

    feat: added static files embed

--- main.go
+++ main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"cartv2/cart/item"
 	"cartv2/cart/shoppinglist"
+	"embed"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
+//go:embed static/*
+var staticFolder embed.FS
+
 func main() {
+	// TODO: .env file(Port)
+
+	staticFS, _ := fs.Sub(staticFolder, "static")
 	port := 420
 
+	http.Handle("/", http.FileServer(http.FS(staticFS)))
 	http.HandleFunc("/shoppinglist", shoppinglist.ChooseHandler)
 	http.HandleFunc("/item", item.ChooseHandler)
 
 	fmt.Printf("Listening on port %d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
 }
