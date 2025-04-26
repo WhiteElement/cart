277fbddc1553589ea2a698da366bb378d38d263a
Manuel Braunwald <manuelbrusche@gmail.com>
2025-04-26 20:39:55 +0200

    feat: added .env file support

--- main.go
+++ main.go
@@ -8,25 +8,38 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 //go:embed static/*
 var staticFolder embed.FS
 
 func main() {
-	// TODO: .env file(Port)
+	args := os.Args
+
+	if len(args) < 2 {
+		log.Fatalln("No .env file as argument provided")
+	}
+
+	err := godotenv.Load(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	staticFS, _ := fs.Sub(staticFolder, "static")
-	port := 420
 
 	http.Handle("/", http.FileServer(http.FS(staticFS)))
 	http.HandleFunc("/shoppinglist", shoppinglist.ChooseHandler)
 	http.HandleFunc("/item", item.ChooseHandler)
 
-	fmt.Printf("Listening on port %d\n", port)
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		log.Println("No PORT Env variable found, setting 420 as a default value")
+		port = 420
+	}
+	log.Printf("Listening on port %d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
-}
-
-func serveIndex(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/index.html")
 }
